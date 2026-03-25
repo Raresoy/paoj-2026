4f67deb44e4a8de14491e5600cd7bf0ac5a4557b
Raresoy <nutizic123@gmail.com>
2026-03-25 22:33:33 +0200

    lab5/playlist

---
+++ src/com/pao/laboratory05/playlist/Song.java
@@ -0,0 +1,8 @@
+package com.pao.laboratory05.playlist;
+
+public record Song(String title, String artist, int durationSeconds) implements Comparable<Song> {
+    @Override
+    public int compareTo(Song val) {
+        return this.title.compareTo(val.title);
+    }
+}
