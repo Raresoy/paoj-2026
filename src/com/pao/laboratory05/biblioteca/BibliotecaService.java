20d9a496e7828b3eea174807b07352e6f1b7e287
Raresoy <nutizic123@gmail.com>
2026-03-25 22:52:10 +0200

    lab5/biblioteca

---
+++ src/com/pao/laboratory05/biblioteca/BibliotecaService.java
@@ -0,0 +1,46 @@
+package com.pao.laboratory05.biblioteca;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BibliotecaService {
+    private Carte[] carti;
+
+    private BibliotecaService() {
+        carti = new Carte[0];
+    }
+
+    private static class Holder {
+        private static final BibliotecaService INSTANCE = new BibliotecaService();
+    }
+
+    public static BibliotecaService getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public void addCarte(Carte carte) {
+        Carte[] newCarti = new Carte[carti.length + 1];
+        System.arraycopy(carti, 0, newCarti, 0, carti.length);
+        newCarti[carti.length] = carte;
+        carti = newCarti;
+
+        System.out.println("Adaugata: " + carte);
+    }
+
+    public void listSortedByRating() {
+        Carte[] copy = carti.clone();
+        Arrays.sort(copy);
+
+        for(Carte c : carti) {
+            System.out.println(c);
+        }
+    }
+
+    public void listSortedBy(Comparator<Carte> comparator) {
+        Carte[] copy = carti.clone();
+        Arrays.sort(copy, comparator);
+        for(Carte c : carti) {
+            System.out.println(c);
+        }
+    }
+}
